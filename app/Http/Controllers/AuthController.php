cd289288f59ce7d77aee96e289ef72da083d32d4
Fred Vanelli <fredsvanelli@gmail.com>
2022-04-12 20:38:07 -0300

    Fixing API response code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,8 +13,7 @@ class AuthController extends Controller
         if (!Auth::attempt($request->only(['email', 'password']))) {
             return response()->json([
                 'message' => 'Invalid credentials',
-                Response::HTTP_UNAUTHORIZED,
-            ]);
+            ], Response::HTTP_UNAUTHORIZED);
         }
 
         $user = Auth::user();
